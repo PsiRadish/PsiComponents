587c9161ff0bc3ab1c9864eafafa39282bf01c5f
PsiRadish <psiradish@comcast.net>
2016-04-28 00:56:26 -0700

    Renamed private method to something more privatey.

--- PsiMenuItem.Constructors.cs
+++ PsiMenuItem.Constructors.cs
@@ -16,7 +16,7 @@ namespace PsiComponents
         {   // == Constructors. Nothing much new here.
         
             // In fact everything new is contained within this method.
-            private void Initialize()
+            private void initNewMembers()
             {
                 isCommandWithDropItems = false;
                 customCheckedImage = null;
@@ -26,26 +26,26 @@ namespace PsiComponents
             /// Initializes a new instance of the ToolStripMenuItem class.
             /// </summary>
             public PsiMenuItem() : base()
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class that displays the specified text.
             /// </summary>
             /// <param name="text">The text to display on the menu item.</param>
             public PsiMenuItem(string text) : base(text)
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class that displays the specified Image.
             /// </summary>
             /// <param name="image">The Image to display on the control.</param>
             public PsiMenuItem(Image image) : base(image)
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class that displays the specified text and image.
             /// </summary>
             /// <param name="text">The text to display on the menu item.</param>
             /// <param name="image">The Image to display on the control.</param>
             public PsiMenuItem(string text, Image image) : base(text, image)
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class that displays the specified text and image and that does
             /// the specified action when the ToolStripMenuItem is clicked.
@@ -54,7 +54,7 @@ namespace PsiComponents
             /// <param name="image">The Image to display on the control.</param>
             /// <param name="onClick">An event handler that raises the Click event when the control is clicked.</param>
             public PsiMenuItem(string text, Image image, EventHandler onClick) : base(text, image, onClick)
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class with the specified name that displays the specified text
             /// and image that does the specified action when the ToolStripMenuItem is clicked. 
@@ -64,7 +64,7 @@ namespace PsiComponents
             /// <param name="onClick">An event handler that raises the Click event when the control is clicked.</param>
             /// <param name="name">The name of the menu item.</param>
             public PsiMenuItem(string text, Image image, EventHandler onClick, string name) : base(text, image, onClick, name)
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class that displays the specified text and image and that
             /// contains the specified ToolStripItem collection. 
@@ -73,7 +73,7 @@ namespace PsiComponents
             /// <param name="image">The Image to display on the control.</param>
             /// <param name="dropDownItems">The menu items to display when the control is clicked.</param>
             public PsiMenuItem(string text, Image image, ToolStripItem[] dropDownItems) : base(text, image, dropDownItems)
-            { this.Initialize(); }
+            { this.initNewMembers(); }
             /// <summary>
             /// Initializes a new instance of the ToolStripMenuItem class that displays the specified text and image, does the
             /// specified action when the ToolStripMenuItem is clicked, and displays the specified shortcut keys.
@@ -85,7 +85,7 @@ namespace PsiComponents
             ///   One of the values of Keys that represents the shortcut key for the ToolStripMenuItem.
             /// </param>
             public PsiMenuItem(string text, Image image, EventHandler onClick, Keys shortcutKeys) : base(text, image, onClick, shortcutKeys)
-            { this.Initialize(); }        
+            { this.initNewMembers(); }        
         }
     }
 }
